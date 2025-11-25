7f0d2348cc89d0c8627c2290f78d08d85d22dc9b
Jomer Jorge <jomer.jorge@infor.com>
2025-11-25 13:04:10 +0800

    feat: create production environment file with API URL configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventory-f13oeaw4j-jomzs-projects-d0964e3c.vercel.app/api'
+  apiUrl: 'https://inventory-three-theta.vercel.app/api'
+  // apiUrl: 'https://inventory-f13oeaw4j-jomzs-projects-d0964e3c.vercel.app/api'
 };
