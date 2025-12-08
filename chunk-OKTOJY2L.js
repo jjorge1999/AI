278993f223024170accdbb6588f245647a8b17b7
Jomer Jorge <jomer.jorge@infor.com>
2025-12-09 07:39:35 +0800

    Auto-generated commit

---
+++ chunk-OKTOJY2L.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QDUU3BBU.js";import"./chunk-MNIEKAC2.js";export{a as LandingComponent};
--- chunk-SMTX7FXP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-6OWT6K6S.js";import"./chunk-MNIEKAC2.js";export{a as LandingComponent};
